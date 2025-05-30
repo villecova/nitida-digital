96622ad93ee12f06a9ca7357ec670753ae7f6301
Juan Pablo Villegas <jp@Juans-MacBook-Air.local>
2025-05-30 11:18:58 -0600

    mobile updates

---
+++ src/components/shared/LottieIcon.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { onMount } from 'svelte';
+  import lottie from 'lottie-web';
+
+  export let src;
+  let container;
+
+  onMount(() => {
+    if (!src || !container) return;
+    lottie.loadAnimation({
+      container,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: src,
+    });
+  });
+</script>
+
+<div bind:this={container} class="md:size-24 p-4 rounded-full bg-purple-200 flex items-center justify-center text-2xl" />
