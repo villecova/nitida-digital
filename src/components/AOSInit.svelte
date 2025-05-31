844dd38d2db00063127d87aee941347658653ed2
Juan Pablo Villegas <jp@Juans-MacBook-Air.local>
2025-05-30 18:49:50 -0600

    animation added

---
+++ src/components/AOSInit.svelte
@@ -0,0 +1,12 @@
+<script>
+  import AOS from 'aos';
+  import 'aos/dist/aos.css';
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    AOS.init({
+      duration: 800,
+      once: true,
+    });
+  });
+</script>
